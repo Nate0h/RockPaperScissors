ad234ada5fba4d33309eb75a3aef3c39d561591b
Nathaniel Colon <nathanielcolon12@gmail.com>
2023-04-11 22:50:13 -0400

    Completed the logic for rock paper scissors

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function getComputerChoice(){
 function getPlayerSelection(){
     let playerSelection = prompt("Rock, Paper, or Scissors?");
     let choice = playerSelection.toLowerCase();
-    console.log(typeof choice);
+    //console.log(typeof choice);
     //console.log(choice);
     while(choice !== "rock" && choice !== "paper" && choice !== "scissors"){
         playerSelection = prompt("Rock, Paper, or Scissors?");
@@ -17,4 +17,47 @@ function getPlayerSelection(){
 
     return choice;
 }
-console.log(getPlayerSelection());
+
+function playRound(playerChoice,computerChoice){
+    let result;
+    switch(playerChoice){
+
+        case "rock":
+            if(computerChoice == "scissors"){
+                result = "You Win!..Rock beats Scissors";
+            }
+            else if(computerChoice == "paper"){
+                result = "You Lose..Paper beats Rock";
+            }
+            else{
+                result = "Tie";
+            }
+            break;
+        case "paper":
+            if(computerChoice == "scissors"){
+                result = "You Lose...Scissors beats Paper";
+            }
+            else if(computerChoice == "rock"){
+                result = "You Win!..Paper beats Rock";
+            }
+            else{
+                result = "Tie";
+            }
+            break;
+        case "scissors":
+            if(computerChoice == "paper"){
+                result = "You Win!..Scissors beats Paper";
+            }
+            else if(computerChoice == "rock"){
+                result = "You Lose..Rock beats Scissors";   
+            }
+            else{
+                result = "Tie";
+            }
+            break;
+    }
+    return result;    
+}
+
+
+console.log(playRound(getPlayerSelection(), getComputerChoice()));
