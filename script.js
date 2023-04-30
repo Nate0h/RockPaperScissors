47e1d1b3fccfa110d51fb6993b0b969acfda5b43
Nathaniel Colon <nathanielcolon12@gmail.com>
2023-04-29 20:22:36 -0400

    Interactive Rock Paper Scissors

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 let choices = new Array("rock","paper","scissors");
+let cpu = 0;
+let player = 0;
 function getComputerChoice(){
     let cpuChoice;
     cpuChoice = choices[Math.floor(Math.random() * choices.length)];
     return cpuChoice;
 }
 
-function getPlayerSelection(){
+/*function getPlayerSelection(){
     let playerSelection = prompt("Rock, Paper, or Scissors?");
     let choice = playerSelection.toLowerCase();
     //console.log(typeof choice);
@@ -16,18 +18,24 @@ function getPlayerSelection(){
     }
 
     return choice;
-}
+}*/
 
-function playRound(playerChoice,computerChoice){
+function playRound(e){
+    
+    let computerChoice = getComputerChoice();
+    let playerChoice = e.target.className;
     let result;
+    
     switch(playerChoice){
 
         case "rock":
             if(computerChoice == "scissors"){
                 result = "You Win!..Rock beats Scissors";
+                player++;
             }
             else if(computerChoice == "paper"){
                 result = "You Lose..Paper beats Rock";
+                cpu++;
             }
             else{
                 result = "Tie";
@@ -36,9 +44,11 @@ function playRound(playerChoice,computerChoice){
         case "paper":
             if(computerChoice == "scissors"){
                 result = "You Lose...Scissors beats Paper";
+                cpu++
             }
             else if(computerChoice == "rock"){
                 result = "You Win!..Paper beats Rock";
+                player++;
             }
             else{
                 result = "Tie";
@@ -47,16 +57,39 @@ function playRound(playerChoice,computerChoice){
         case "scissors":
             if(computerChoice == "paper"){
                 result = "You Win!..Scissors beats Paper";
+                player++;
             }
             else if(computerChoice == "rock"){
-                result = "You Lose..Rock beats Scissors";   
+                result = "You Lose..Rock beats Scissors"; 
+                cpu++;  
             }
             else{
                 result = "Tie";
             }
             break;
     }
-    return result;    
+    const results = document.getElementById('results');
+    const para = document.createElement("p");
+    result += ` player: ${player} - cpu: ${cpu}`;  
+    para.textContent = result;
+    results.appendChild(para);
+
+    if(cpu == 5){
+        const final = document.createElement("p");
+        final.textContent = "CPU Wins the Game";
+        results.appendChild(final);
+        player = 0;
+        cpu = 0;
+    }
+    if(player == 5){
+        const final = document.createElement("p");
+        final.textContent = "You Win the Game!"
+        results.appendChild(final);
+        player = 0;
+        cpu = 0;
+    }
+    console.log(result);
+    //return result;   
 }
 
 function game(){
@@ -86,5 +119,10 @@ function game(){
     }
 }
 
+//DOM Section 
+const buttons = document.querySelectorAll('button');
+console.log(buttons.length);
 
-console.log(game());
+//event listener
+buttons.forEach(button => button.addEventListener('click', playRound));
+//console.log(game());
